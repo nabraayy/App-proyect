d74f1854210120ecb80fcb3c8e9a2df606bbae79
Nay <garciatarraganayara@gmail.com>
2025-01-28 17:10:52 +0100

    tt

--- app/lib/actions.js
+++ app/lib/actions.js
@@ -1,14 +1,14 @@
 'use server'
 export async function createPost(formData) {
     //insert into post
-//     const{url}= await put(
-//         'media', 
-//         formData.get("media"),
-//         {access:'public'}
-// )
+    const{url}= await put(
+        'media', 
+        formData.get("media"),
+        {access:'public'}
+    )
 
-//     const content = formData.get('content')
-//     await sql'INSERT INTO POST(content,url) VALUES (${formData.get('content')}, ${url})';
+    const content = formData.get('content');
+    await sql('INSERT INTO POST(content,url) VALUES (${formData.get(content)}, ${url})');
 
     
 }
--- app/seed/page.js
+++
@@ -1,5 +0,0 @@
-import{sql} from '@vercel/postgres'
-export default async()=>{
-    await sql'CREATE TABLE POST(id UUID DEFAULT uuid_generate_v4() PRIMARY KEY, content TEXT,url TEXT)'
-    return <p>Database seed</p>
-}
