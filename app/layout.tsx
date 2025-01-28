176f57369752f7e4f414dcf27d582ffb0c83e49b
Nay <garciatarraganayara@gmail.com>
2025-01-28 17:30:06 +0100

    yyyy

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,10 @@
-export default function RootLayout({children}) {
+import React from 'react';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode; // Define el tipo para children
+}) {
   return (
     <html lang="en">
       <body>{children}</body>
